c12b51e8419c874d01a709d1f6f7209a6aae078e
brianmuigai96 <brian.muigai@student.moringaschool.com>
2022-01-19 15:15:58 +0300

    adding heroku database

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -2,6 +2,8 @@ import org.sql2o.Sql2o;
 
 /*connecting the application with the database locally*/
 public class DB {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "softwareengineering", "1234");
+//    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "softwareengineering", "1234");
+
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-52-45-183-77.compute-1.amazonaws.com:5432/d8988685voolpb", "qngbbkodoxyjnl", "74ee1bf87f55b6dc628d625db39a1b51d3065344fc2ff7f1c8fc49abda250096");
 
 }
--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -6,7 +6,9 @@ import org.sql2o.Connection;
 public class DatabaseRule extends ExternalResource {
     @Override
     public void before() {
-        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "softwareengineering", "1234");
+//        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "softwareengineering", "1234");
+        public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-52-45-183-77.compute-1.amazonaws.com:5432/d8988685voolpb", "qngbbkodoxyjnl", "74ee1bf87f55b6dc628d625db39a1b51d3065344fc2ff7f1c8fc49abda250096");
+
     }
 
     @Override
