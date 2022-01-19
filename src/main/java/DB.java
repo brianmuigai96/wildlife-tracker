a1563915179c6545d7b2c73580e93c394845423e
brianmuigai96 <brian.muigai@student.moringaschool.com>
2022-01-19 17:40:15 +0300

    adding changes

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -4,6 +4,6 @@ import org.sql2o.Sql2o;
 public class DB {
 //    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "softwareengineering", "1234");
 
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-52-45-183-77.compute-1.amazonaws.com:5432/d8988685voolpb", "qngbbkodoxyjnl", "74ee1bf87f55b6dc628d625db39a1b51d3065344fc2ff7f1c8fc49abda250096");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-52-45-183-77.compute-1.amazonaws.com:5432/d8988685voolpb?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory", "qngbbkodoxyjnl", "74ee1bf87f55b6dc628d625db39a1b51d3065344fc2ff7f1c8fc49abda250096");
 
 }
